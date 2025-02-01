216e76bca135ed7a0350c57163d6503b5d0a1d02
Greg Ennis <greg@innodroid.com>
2025-02-01 10:01:03 -0500

    Initial Commit

---
+++ Brownie/BrownieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrownieApp.swift
+//  Brownie
+//
+//  Created by Gregory Ennis on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BrownieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
